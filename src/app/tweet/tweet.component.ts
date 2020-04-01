c90aa181a68d446adc2352b12e51acce092e3d88
angarcia20 <angarcia20@poligran.edu.co>
2020-04-01 03:31:32 -0500

    faltan estilos

---
+++ src/app/tweet/tweet.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {Tweet} from '../models/tweet.model'
+
+@Component({
+  selector: 'app-tweet',
+  templateUrl: './tweet.component.html',
+  styleUrls: ['./tweet.component.css']
+})
+export class TweetComponent implements OnInit {
+  listaTweets = [];
+  nuevotweet : Tweet;
+
+  constructor() { 
+      this.nuevotweet={
+      id: this.listaTweets.length,
+      texto: "",
+      autor : ""
+    }
+    console.log("LISTA ", this.listaTweets);
+
+
+  }
+  adicionartweet(){
+    this.listaTweets.push(this.nuevotweet);
+    this.nuevotweet = new Tweet;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
